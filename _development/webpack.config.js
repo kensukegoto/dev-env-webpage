3b8055d258e0ce80d7a00394d7e2122b36d11bcf
ws004 <n_ope@webstaff.jp>
2019-10-03 10:02:56 +0900

    [追加] 開発環境作成　by後藤

---
+++ _development/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = (mode = "development") => {
+    
+  return {
+      mode: mode,
+      entry: {
+          "js/index": "./es6/index.js",
+      },
+      output:{
+          filename: "[name].bundle.js",
+      },
+      module: {
+          rules: [
+          { 
+              test: /\.js$/, 
+              loader: "babel-loader",
+              include:[
+                /es6/
+              ]
+          }
+          ]
+      },
+      devtool: (mode === "development") ? "inline-source-map" : false
+  }
+}
